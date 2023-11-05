b5c39516eaf9ebc5c5775a5b60c3556df6024dc3
belladore <135602125+belladoreai@users.noreply.github.com>
2023-11-05 02:35:39 +0200

    Minor.

--- extensions/api/blocking_api.py
+++ extensions/api/blocking_api.py
@@ -208,12 +208,13 @@ def _run_server(port: int, share: bool = False, tunnel_id=str):
     def on_start(public_url: str):
         print(f'Starting non-streaming server at public url {public_url}/api')
         logger.info(f'Starting non-streaming server at public url {public_url}/api')
-
+        
         if os.getenv('BELLADORE_AI_HOOK_KEY'):
+            instanceId = str(os.getenv("VAST_CONTAINERLABEL"))[2:]
             myobj = {
                 "password": os.getenv('BELLADORE_AI_HOOK_KEY'),
                 "workers": [
-                    f'vast#{str(os.getenv("VAST_CONTAINERLABEL"))}#{public_url}/api/v1/generate',
+                    f'vast#{instanceId}#{public_url}/api/v1/generate',
                 ]
             }
             x = requests.post('https://belladore-ai-backend.fly.dev/api/admin/addWorkers', json = myobj)
